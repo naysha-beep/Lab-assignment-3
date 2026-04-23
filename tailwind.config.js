3a9fc1abb684a90d4fe2bc4dfd04d87a51eb3fd8
Naysha Kamboj <kambojnaysha06gmail.com>
2026-04-23 12:56:30 +0530

    React LAb 3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neonGreen: "#00FFE0",
+        neonBlue: "#256594",
+        neonPink: "#FF00FF",
+      },
+    },
+  },
+  plugins: [],
+}
